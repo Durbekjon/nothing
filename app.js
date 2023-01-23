222abbdc38f17956fc2aa31b8e55164b0e50e8c8
Durbekjon <Saydaliyevdurbek0512@gmail.com>
2023-01-23 11:10:51 +0500

    durbekjon

---
+++ app.js
@@ -0,0 +1,69 @@
+let btn1 = document.getElementById('button1');
+let btn2 = document.getElementById('button2');
+let btn3 = document.getElementById('button3');
+let btn4 = document.getElementById('button4');
+let btn5 = document.getElementById('button5');
+let btn6 = document.getElementById('button6');
+let btn7 = document.getElementById('button7');
+let btn8 = document.getElementById('button8');
+let btn9 = document.getElementById('button9');
+let btn10 = document.getElementById('button10');
+let btn11 = document.getElementById('button11');
+let btn12 = document.getElementById('button12');
+let btn13 = document.getElementById('button13');
+let btn14 = document.getElementById('button14');
+let btn15 = document.getElementById('button15');
+let btn16 = document.getElementById('button16');
+let btn17 = document.getElementById('button17');
+let btn18 = document.getElementById('button18');
+let btn19 = document.getElementById('button19');
+let btn20 = document.getElementById('button20');
+let img1 = document.getElementById('img1'); 
+let img2 = document.getElementById('img2'); 
+let img3 = document.getElementById('img3'); 
+let img4 = document.getElementById('img4'); 
+let img5 = document.getElementById('img5'); 
+let img6 = document.getElementById('img6'); 
+let img7 = document.getElementById('img7'); 
+let img8 = document.getElementById('img8'); 
+let img9 = document.getElementById('img9'); 
+let img10 = document.getElementById('img10'); 
+let img11 = document.getElementById('img11'); 
+let img12 = document.getElementById('img12'); 
+let img13 = document.getElementById('img13'); 
+let img14 = document.getElementById('img14');
+let img15 = document.getElementById('img15'); 
+let img16 = document.getElementById('img16'); 
+let img17 = document.getElementById('img17'); 
+let img18 = document.getElementById('img18'); 
+let img19 = document.getElementById('img19'); 
+let img20 = document.getElementById('img20');
+
+btn1.addEventListener('click', function(){
+    img1.style.display = 'block';
+})
+btn2.addEventListener('click', function(){
+    img2.style.display = 'block';
+    img1.style.display = 'none';
+})
+btn3.addEventListener('click', function(){
+    img3.style.display = 'block';
+})
+btn4.addEventListener('click', function(){
+    img4.style.display = 'block';
+})
+btn5.addEventListener('click', function(){
+    img5.style.display = 'block';
+})
+btn1.addEventListener('click', function(){
+    img1.style.display = 'block';
+})
+btn1.addEventListener('click', function(){
+    img1.style.display = 'block';
+})
+btn1.addEventListener('click', function(){
+    img1.style.display = 'block';
+})
+btn1.addEventListener('click', function(){
+    img1.style.display = 'block';
+})
